1427969853a04789bb4f4b8e6447424cd7f9d749
Zein Ersyad <zeinersyad@gmail.com>
2022-12-24 13:51:18 +0700

    fix capitalize on image file logo

--- src/routes/u/+layout.svelte
+++ src/routes/u/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import '../../app.css';
 	import '../styles.css';
-    import inving from '../../lib/images/inving.png';
+    import inving from '$lib/images/inving.png';
 </script>
 
 <div class="app">
