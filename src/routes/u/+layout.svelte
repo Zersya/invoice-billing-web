7b058a1f845b24e2d0581c56b905025d03174e7f
Zein Ersyad <zeinersyad@gmail.com>
2023-01-09 08:33:05 +0700

    fix visibility of hamburger menu icon on mobile

--- src/routes/u/+layout.svelte
+++ src/routes/u/+layout.svelte
@@ -6,7 +6,11 @@
 	import Breadcumbs from './Breadcumbs.svelte';
 	import { themeChange } from 'theme-change';
 
+	let isMobile = false;
+
 	onMount(() => {
+		isMobile = window.innerWidth < 768;
+
 		themeChange(false);
 	});
 </script>
@@ -43,36 +47,40 @@
 	<div class="drawer drawer-mobile">
 		<input id="my-drawer" type="checkbox" class="drawer-toggle" />
 		<div class="drawer-content px-10 py-2">
-			<label for="my-drawer" class="btn btn-ghost btn-circle drawer-button">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path stroke-width=".25" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg
-				>
-			</label>
+			{#if isMobile}
+				<label for="my-drawer" class="btn btn-ghost btn-circle drawer-button">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path stroke-width=".25" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg
+					>
+				</label>
+			{/if}
 			<Breadcumbs />
 			<slot />
 		</div>
-		<div class="drawer-side">
+		<div class="drawer-side drop-shadow">
 			<label for="my-drawer" class="drawer-overlay" />
 			<ul class="menu p-4 w-80 bg-base-100 text-base-content">
 				<div class="flex justify-between mb-3">
 					<img src={inving} width="45" alt="inving" />
 
-					<label for="my-drawer" class="btn btn-ghost btn-circle drawer-button">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							><path
-								stroke-width=".25"
-								d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
-							/></svg
-						>
-					</label>
+					{#if isMobile}
+						<label for="my-drawer" class="btn btn-ghost btn-circle drawer-button">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								><path
+									stroke-width=".25"
+									d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+								/></svg
+							>
+						</label>
+					{/if}
 				</div>
 				<li>
 					<a href="/u"
@@ -110,14 +118,14 @@
 					class="flex flex-row justify-around"
 					style="position:fixed; bottom: 3rem; right: 0; left: 0;"
 				>
-					<select class="input input-bordered w-9/12 max-w-xs mx-auto" data-choose-theme>
+					<select class="select input-bordered w-9/12 max-w-xs mx-auto" data-choose-theme>
 						<option value="mytheme">Default</option>
 						<option value="dark">Dark</option>
 						<option value="cupcake">Cupcake</option>
 						<option value="forest">Forest</option>
 					</select>
 					<form method="POST" action="/u?/signout">
-						<button class="btn btn-ghost btn-circle btn-primary">
+						<button class="btn btn-ghost btn-circle btn-primary mr-2">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								class="h-5 w-5"
