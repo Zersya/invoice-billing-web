d408d21222bd1f6328dcea916edc4f75276257a1
Zein Ersyad <zeinersyad@gmail.com>
2023-02-10 10:54:36 +0700

    add condition no schedules found

--- src/routes/u/+page.svelte
+++ src/routes/u/+page.svelte
@@ -7,5 +7,12 @@
 </script>
 
 <AuthenticatedPage>
-	<Calendar isFullTitle={true} schedules={data.schedules} />
+	{#if !data.schedules}
+		<div class="flex flex-col items-center justify-center h-full">
+			<div class="prose prose-xl">No schedules found</div>
+			<div class="prose prose-slate prose-sm mb-5">Please create a schedule to get started</div>
+		</div>
+	{:else}
+		<Calendar isFullTitle={true} schedules={data.schedules} />
+	{/if}
 </AuthenticatedPage>
